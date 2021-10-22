7d981096f65a466d3d725597334990569ccfcaf5
robot9001 <robo9k@symlink.io>
2021-10-22 21:26:23 +0200

    Use feature names compatible with `system-deps` crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 extern crate libc;
-#[cfg(feature = "libmagic-abi-v520")]
+#[cfg(feature = "v5-20")]
 use libc::c_void;
 use libc::{c_char, c_int, size_t};
 
@@ -26,11 +26,11 @@ pub const MAGIC_ERROR: c_int = 0x000_0200;
 pub const MAGIC_MIME_ENCODING: c_int = 0x000_0400;
 pub const MAGIC_MIME: c_int = MAGIC_MIME_TYPE | MAGIC_MIME_ENCODING;
 pub const MAGIC_APPLE: c_int = 0x00_0800;
-#[cfg(feature = "libmagic-abi-v523")]
+#[cfg(feature = "v5-23")]
 pub const MAGIC_EXTENSION: c_int = 0x100_0000;
-#[cfg(feature = "libmagic-abi-v523")]
+#[cfg(feature = "v5-23")]
 pub const MAGIC_COMPRESS_TRANSP: c_int = 0x200_0000;
-#[cfg(feature = "libmagic-abi-v523")]
+#[cfg(feature = "v5-23")]
 pub const MAGIC_NODESC: c_int = MAGIC_EXTENSION | MAGIC_MIME | MAGIC_APPLE;
 
 pub const MAGIC_NO_CHECK_COMPRESS: c_int = 0x000_1000;
@@ -40,16 +40,16 @@ pub const MAGIC_NO_CHECK_APPTYPE: c_int = 0x000_8000;
 pub const MAGIC_NO_CHECK_ELF: c_int = 0x001_0000;
 pub const MAGIC_NO_CHECK_TEXT: c_int = 0x002_0000;
 pub const MAGIC_NO_CHECK_CDF: c_int = 0x004_0000;
-#[cfg(feature = "libmagic-abi-v538")]
+#[cfg(feature = "v5-38")]
 pub const MAGIC_NO_CHECK_CSV: c_int = 0x008_0000;
 pub const MAGIC_NO_CHECK_TOKENS: c_int = 0x010_0000;
 pub const MAGIC_NO_CHECK_ENCODING: c_int = 0x020_0000;
-#[cfg(feature = "libmagic-abi-v535")]
+#[cfg(feature = "v5-35")]
 pub const MAGIC_NO_CHECK_JSON: c_int = 0x040_0000;
 
-#[cfg(all(feature = "libmagic-abi-v505", not(feature = "libmagic-abi-v510")))]
+#[cfg(all(feature = "v5-05", not(feature = "v5-10")))]
 pub const MAGIC_NO_CHECK_BUILTIN: c_int = 0x3f_b000;
-#[cfg(all(feature = "libmagic-abi-v510", not(feature = "libmagic-abi-v535")))]
+#[cfg(all(feature = "v5-10", not(feature = "v5-35")))]
 pub const MAGIC_NO_CHECK_BUILTIN: c_int = MAGIC_NO_CHECK_COMPRESS |
 MAGIC_NO_CHECK_TAR      |
 // MAGIC_NO_CHECK_SOFT  |
@@ -59,7 +59,7 @@ MAGIC_NO_CHECK_TEXT     |
 MAGIC_NO_CHECK_CDF      |
 MAGIC_NO_CHECK_TOKENS   |
 MAGIC_NO_CHECK_ENCODING;
-#[cfg(all(feature = "libmagic-abi-v535", not(feature = "libmagic-abi-v538")))]
+#[cfg(all(feature = "v5-35", not(feature = "v5-38")))]
 pub const MAGIC_NO_CHECK_BUILTIN: c_int = MAGIC_NO_CHECK_COMPRESS |
 MAGIC_NO_CHECK_TAR      |
 // MAGIC_NO_CHECK_SOFT  |
@@ -70,7 +70,7 @@ MAGIC_NO_CHECK_CDF      |
 MAGIC_NO_CHECK_TOKENS   |
 MAGIC_NO_CHECK_ENCODING |
 MAGIC_NO_CHECK_JSON;
-#[cfg(feature = "libmagic-abi-v538")]
+#[cfg(feature = "v5-38")]
 pub const MAGIC_NO_CHECK_BUILTIN: c_int = MAGIC_NO_CHECK_COMPRESS |
 MAGIC_NO_CHECK_TAR      |
 // MAGIC_NO_CHECK_SOFT  |
@@ -95,54 +95,54 @@ pub const MAGIC_NO_CHECK_TROFF: c_int = 0x00_0000;
 
 // TODO: MAGIC_SNPRINTB bytes
 
-#[cfg(feature = "libmagic-abi-v521")]
+#[cfg(feature = "v5-21")]
 pub const MAGIC_PARAM_INDIR_MAX: c_int = 0;
-#[cfg(feature = "libmagic-abi-v521")]
+#[cfg(feature = "v5-21")]
 pub const MAGIC_PARAM_NAME_MAX: c_int = 1;
-#[cfg(feature = "libmagic-abi-v521")]
+#[cfg(feature = "v5-21")]
 pub const MAGIC_PARAM_ELF_PHNUM_MAX: c_int = 2;
-#[cfg(feature = "libmagic-abi-v521")]
+#[cfg(feature = "v5-21")]
 pub const MAGIC_PARAM_ELF_SHNUM_MAX: c_int = 3;
-#[cfg(feature = "libmagic-abi-v522")]
+#[cfg(feature = "v5-22")]
 pub const MAGIC_PARAM_ELF_NOTES_MAX: c_int = 4;
-#[cfg(feature = "libmagic-abi-v525")]
+#[cfg(feature = "v5-25")]
 pub const MAGIC_PARAM_REGEX_MAX: c_int = 5;
-#[cfg(feature = "libmagic-abi-v527")]
+#[cfg(feature = "v5-27")]
 pub const MAGIC_PARAM_BYTES_MAX: c_int = 6;
-#[cfg(feature = "libmagic-abi-v540")]
+#[cfg(feature = "v5-40")]
 pub const MAGIC_PARAM_ENCODING_MAX: c_int = 7;
 
 // NOTE: the following are from `file.h`, but part of `magic.h` API
-#[cfg(feature = "libmagic-abi-v504")]
+#[cfg(feature = "v5-04")]
 pub const FILE_LOAD: c_int = 0;
-#[cfg(feature = "libmagic-abi-v504")]
+#[cfg(feature = "v5-04")]
 pub const FILE_CHECK: c_int = 1;
-#[cfg(feature = "libmagic-abi-v504")]
+#[cfg(feature = "v5-04")]
 pub const FILE_COMPILE: c_int = 2;
-#[cfg(feature = "libmagic-abi-v505")]
+#[cfg(feature = "v5-05")]
 pub const FILE_LIST: c_int = 3;
 
 extern "C" {
     pub fn magic_open(flags: c_int) -> *const Magic;
     pub fn magic_close(cookie: *const Magic);
 
-    #[cfg(feature = "libmagic-abi-v504")]
+    #[cfg(feature = "v5-04")]
     pub fn magic_getpath(magicfile: *const c_char, action: c_int) -> *const c_char;
     pub fn magic_file(cookie: *const Magic, filename: *const c_char) -> *const c_char;
     pub fn magic_descriptor(cookie: *const Magic, fd: c_int) -> *const c_char;
     pub fn magic_buffer(cookie: *const Magic, buffer: *const u8, length: size_t) -> *const c_char;
 
     pub fn magic_error(cookie: *const Magic) -> *const c_char;
-    #[cfg(feature = "libmagic-abi-v532")]
+    #[cfg(feature = "v5-32")]
     pub fn magic_getflags(cookie: *const Magic) -> c_int;
     #[must_use]
     pub fn magic_setflags(cookie: *const Magic, flags: c_int) -> c_int;
 
-    #[cfg(feature = "libmagic-abi-v513")]
+    #[cfg(feature = "v5-13")]
     pub fn magic_version() -> c_int;
     #[must_use]
     pub fn magic_load(cookie: *const Magic, filename: *const c_char) -> c_int;
-    #[cfg(feature = "libmagic-abi-v520")]
+    #[cfg(feature = "v5-20")]
     #[must_use]
     pub fn magic_load_buffers(
         cookie: *const Magic,
@@ -155,15 +155,15 @@ extern "C" {
     pub fn magic_compile(cookie: *const Magic, filename: *const c_char) -> c_int;
     #[must_use]
     pub fn magic_check(cookie: *const Magic, filename: *const c_char) -> c_int;
-    #[cfg(feature = "libmagic-abi-v505")]
+    #[cfg(feature = "v5-05")]
     #[must_use]
     pub fn magic_list(cookie: *const Magic, filename: *const c_char) -> c_int;
     pub fn magic_errno(cookie: *const Magic) -> *const c_int;
 
-    #[cfg(feature = "libmagic-abi-v521")]
+    #[cfg(feature = "v5-21")]
     #[must_use]
     pub fn magic_setparam(cookie: *const Magic, param: c_int, value: *const c_void) -> c_int;
-    #[cfg(feature = "libmagic-abi-v521")]
+    #[cfg(feature = "v5-21")]
     #[must_use]
     pub fn magic_getparam(cookie: *const Magic, param: c_int, value: *mut c_void) -> c_int;
 }
@@ -189,7 +189,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v504")]
+    #[cfg(feature = "v5-04")]
     #[test]
     fn test_magic_getpath() {
         unsafe {
@@ -225,7 +225,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v532")]
+    #[cfg(feature = "v5-32")]
     #[test]
     fn test_magic_getflags() {
         unsafe {
@@ -240,7 +240,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v513")]
+    #[cfg(feature = "v5-13")]
     #[test]
     fn test_magic_version() {
         unsafe {
@@ -255,7 +255,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v520")]
+    #[cfg(feature = "v5-20")]
     #[test]
     fn test_magic_load_buffers() {
         unsafe {
@@ -277,7 +277,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v505")]
+    #[cfg(feature = "v5-05")]
     #[test]
     fn test_magic_list() {
         unsafe {
@@ -292,7 +292,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v521")]
+    #[cfg(feature = "v5-21")]
     #[test]
     fn test_magic_setparam() {
         unsafe {
@@ -300,7 +300,7 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "libmagic-abi-v521")]
+    #[cfg(feature = "v5-21")]
     #[test]
     fn test_magic_getparam() {
         unsafe {
